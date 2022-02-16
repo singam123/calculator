59ef8321ad1df421b8e75ef53ee3fd5a458e51d9
Singam Pardhasaradh <singam.pardhasaradh@kyndryl.com>
2022-02-16 19:06:17 +0530

    added mycalc code

---
+++ mycalc/clc.js
@@ -0,0 +1,61 @@
+var numKeys = [];
+var opns = [];
+var output = "";
+var res = document.querySelector("#resultArea");
+var clrBtn = document.querySelector("#clrTxt");
+var delBtn = document.querySelector("#del");
+var eqBtn = document.querySelector("#eq");
+var decPoint = document.querySelector("#decp");
+var opnSyms = ["+", "-", "*", "/","%"];
+
+for(var i=0;i<=9;i++){
+	(function(i){
+    	qs = "#num" + i;
+		numKeys.push(document.querySelector(qs));
+		numKeys[i].addEventListener("click", function(){
+			if (res.textContent.length >9){
+					alert("input digits limit reached")
+					}
+			else{
+			res.textContent += i;
+			}
+		});
+  	}(i));
+}
+
+for(var i=0;i<=4;i++){
+	(function(i){
+    	qs = "#op" + i;
+		opns.push(document.querySelector(qs));
+		opns[i].addEventListener("click", function(){
+			res.textContent += opnSyms[i];
+		});
+  	}(i));
+}
+
+clrBtn.addEventListener("click", function(){
+	res.textContent = "";
+});
+
+delBtn.addEventListener("click", function(){
+    res.textContent = res.textContent.substring(0, res.textContent.length - 1);
+});
+
+decPoint.addEventListener("click", function(){
+	res.textContent += ".";
+});
+
+eqBtn.addEventListener("click", function(){
+	try{
+		res.textContent = eval(res.textContent);
+		if (res.textContent.length >9){
+					alert("Output digits limit reached")
+					}
+			else{
+			res.textContent;
+			}
+	}
+	catch(e){
+		res.textContent = "Invalid Syntax";
+	}
+});
